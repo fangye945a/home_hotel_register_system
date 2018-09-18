c365e628fd4bb59f79b3e2859a79b8a55ca8a730
fangye <fangye945@qq.com>
2018-09-19 01:11:18 +0800

    完成输入手机号及验证码界面逻辑，增加入住无预订信息界面

--- homestay/home_hotel.cpp
+++ homestay/home_hotel.cpp
@@ -6,14 +6,70 @@ home_hotel::home_hotel(QWidget *parent) :
     ui(new Ui::home_hotel)
 {
     ui->setupUi(this);
+    home_hotel_init();
     QPalette palette;
     palette.setBrush(QPalette::Background, QBrush(QPixmap("../homestay/pictures/首界面.png")));
     this->setPalette(palette);
+    ui->phone_number->installEventFilter(this);  //声明机制的存在
+    ui->test_code->installEventFilter(this);
 }
 
 home_hotel::~home_hotel()
 {
     delete ui;
+}
+
+bool home_hotel::eventFilter(QObject *obj, QEvent *event)
+{
+    if( event->type() == QEvent::MouseButtonPress)
+    {
+        if(ui->test_code->text().isEmpty())
+        {
+            ui->test_code->setText("请输入验证码");
+            ui->test_code->setStyleSheet("#test_code{color: rgb(184, 184, 184);"
+                                            "font: 75 16pt \"Microsoft YaHei UI Light\";"
+                                            "border-image: url(:/new/prefix1/pictures/输入文本框.png);}");
+        }
+        if(ui->phone_number->text().isEmpty())
+        {
+            ui->phone_number->setText("请输入手机号");
+            ui->phone_number->setStyleSheet("#phone_number{color: rgb(184, 184, 184);"
+                                            "font: 75 16pt \"Microsoft YaHei UI Light\";"
+                                            "border-image: url(:/new/prefix1/pictures/输入文本框.png);}");
+        }
+        QMouseEvent *me = (QMouseEvent*)event;
+        if( me->button() == Qt::LeftButton )
+        {
+            if( obj == ui->phone_number )
+            {
+                if(ui->phone_number->text() == "请输入手机号")
+                {
+                    ui->phone_number->clear();
+                    ui->phone_number->setStyleSheet("#phone_number{color: rgb(0,0,0);"
+                                                    "font: 75 16pt \"Microsoft YaHei UI Light\";"
+                                                    "border-image: url(:/new/prefix1/pictures/输入文本框.png);}");
+                }
+                focus_flag = 0;
+            }
+            else if(obj == ui->test_code )
+            {
+                if(ui->test_code->text() == "请输入验证码")
+                {
+                    ui->test_code->clear();
+                    ui->test_code->setStyleSheet("#test_code{color: rgb(0,0,0);"      //"color: rgb(184, 184, 184);"
+                                                 "font: 75 16pt \"Microsoft YaHei UI Light\";"
+                                                 "border-image: url(:/new/prefix1/pictures/输入文本框.png);}");
+                }
+                focus_flag = 1;
+            }
+        }
+    }
+    return QWidget::eventFilter(obj,event);
+}
+
+void home_hotel::home_hotel_init()
+{
+    focus_flag = -1;
 }
 
 void home_hotel::on_check_in_clicked()
@@ -31,4 +87,181 @@ void home_hotel::on_exit_clicked()
     }else
         i = 0;
     ui->stackedWidget->setCurrentIndex(i);
+}
+
+void home_hotel::set_lineEdit_text(int opt_code)
+{
+    qDebug()<<"set_lineEdit_text"<<" focus_flag="<<focus_flag<<" opt_code="<<opt_code;
+    if(focus_flag == 0)
+    {
+        QString msg = ui->phone_number->text();
+        switch (opt_code)
+        {
+            case -2:     //清空
+                ui->phone_number->clear();
+                break;
+            case -1:     //删除
+                msg.remove(msg.length()-1,1);
+                ui->phone_number->setText(msg);
+                break;
+            case 0:
+                msg.append("0");
+                ui->phone_number->setText(msg);
+                break;
+            case 1:
+                msg.append("1");
+                ui->phone_number->setText(msg);
+                break;
+            case 2:
+                msg.append("2");
+                ui->phone_number->setText(msg);
+                break;
+            case 3:
+                msg.append("3");
+                ui->phone_number->setText(msg);
+                break;
+            case 4:
+                msg.append("4");
+                ui->phone_number->setText(msg);
+                break;
+            case 5:
+                msg.append("5");
+                ui->phone_number->setText(msg);
+                break;
+            case 6:
+                msg.append("6");
+                ui->phone_number->setText(msg);
+                break;
+            case 7:
+                msg.append("7");
+                ui->phone_number->setText(msg);
+                break;
+            case 8:
+                msg.append("8");
+                ui->phone_number->setText(msg);
+                break;
+            case 9:
+                msg.append("9");
+                ui->phone_number->setText(msg);
+                break;
+        default: qDebug()<<"Unknow opt code = "<<opt_code;
+            break;
+        }
+    }
+    else if(focus_flag == 1)
+    {
+        QString msg = ui->test_code->text();
+        switch (opt_code)
+        {
+            case -2:     //清空
+                ui->test_code->clear();
+                break;
+            case -1:     //删除
+                msg.remove(msg.length()-1,1);
+                ui->test_code->setText(msg);
+                break;
+            case 0:
+                msg.append("0");
+                ui->test_code->setText(msg);
+                break;
+            case 1:
+                msg.append("1");
+                ui->test_code->setText(msg);
+                break;
+            case 2:
+                msg.append("2");
+                ui->test_code->setText(msg);
+                break;
+            case 3:
+                msg.append("3");
+                ui->test_code->setText(msg);
+                break;
+            case 4:
+                msg.append("4");
+                ui->test_code->setText(msg);
+                break;
+            case 5:
+                msg.append("5");
+                ui->test_code->setText(msg);
+                break;
+            case 6:
+                msg.append("6");
+                ui->test_code->setText(msg);
+                break;
+            case 7:
+                msg.append("7");
+                ui->test_code->setText(msg);
+                break;
+            case 8:
+                msg.append("8");
+                ui->test_code->setText(msg);
+                break;
+            case 9:
+                msg.append("9");
+                ui->test_code->setText(msg);
+                break;
+        default: qDebug()<<"Unknow opt code = "<<opt_code;
+            break;
+        }
+    }
+}
+
+void home_hotel::on_num_clean_clicked()
+{
+    set_lineEdit_text(-2);
+}
+
+void home_hotel::on_num_del_clicked()
+{
+    set_lineEdit_text(-1);
+}
+
+void home_hotel::on_num_0_clicked()
+{
+    set_lineEdit_text(0);
+}
+
+void home_hotel::on_num_1_clicked()
+{
+    set_lineEdit_text(1);
+}
+
+void home_hotel::on_num_2_clicked()
+{
+    set_lineEdit_text(2);
+}
+
+void home_hotel::on_num_3_clicked()
+{
+    set_lineEdit_text(3);
+}
+
+void home_hotel::on_num_4_clicked()
+{
+    set_lineEdit_text(4);
+}
+
+void home_hotel::on_num_5_clicked()
+{
+    set_lineEdit_text(5);
+}
+
+void home_hotel::on_num_6_clicked()
+{
+    set_lineEdit_text(6);
+}
+
+void home_hotel::on_num_7_clicked()
+{
+    set_lineEdit_text(7);
+}
+
+void home_hotel::on_num_8_clicked()
+{
+    set_lineEdit_text(8);
+}
+
+void home_hotel::on_num_9_clicked()
+{
+    set_lineEdit_text(9);
 }
--- homestay/home_hotel.h
+++ homestay/home_hotel.h
@@ -2,6 +2,8 @@
 #define HOME_HOTEL_H
 
 #include <QWidget>
+#include <QDebug>
+#include <QMouseEvent>
 
 namespace Ui {
 class home_hotel;
@@ -14,14 +16,42 @@ class home_hotel : public QWidget
 public:
     explicit home_hotel(QWidget *parent = 0);
     ~home_hotel();
-
+    bool eventFilter(QObject *obj, QEvent *event);
+    void home_hotel_init();
 private slots:
     void on_check_in_clicked();
 
     void on_exit_clicked();
 
+    void set_lineEdit_text(int opt_code);
+
+    void on_num_clean_clicked();
+
+    void on_num_0_clicked();
+
+    void on_num_del_clicked();
+
+    void on_num_1_clicked();
+
+    void on_num_2_clicked();
+
+    void on_num_3_clicked();
+
+    void on_num_4_clicked();
+
+    void on_num_5_clicked();
+
+    void on_num_6_clicked();
+
+    void on_num_7_clicked();
+
+    void on_num_8_clicked();
+
+    void on_num_9_clicked();
+
 private:
     Ui::home_hotel *ui;
+    int focus_flag;   //-1 无焦点 0 电话号码  1 验证码
 };
 
 #endif // HOME_HOTEL_H
