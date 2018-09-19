391978be62877f55d9c116cd9dadd6d0e9cc95ac
fangye <fangye945@qq.com>
2018-09-19 20:48:01 +0800

    完成主页面天气和时间显示功能

--- homestay/home_hotel.cpp
+++ homestay/home_hotel.cpp
@@ -6,12 +6,16 @@ home_hotel::home_hotel(QWidget *parent) :
     ui(new Ui::home_hotel)
 {
     ui->setupUi(this);
-    home_hotel_init();
-    QPalette palette;
+
+    QPalette palette;   //显示首界面背景图
     palette.setBrush(QPalette::Background, QBrush(QPixmap("../homestay/pictures/首界面.png")));
     this->setPalette(palette);
-    ui->phone_number->installEventFilter(this);  //声明机制的存在
+
+    ui->phone_number->installEventFilter(this);  //声明QLineEdit focus机制的存在
     ui->test_code->installEventFilter(this);
+
+    signal_slots_connect(); //连接信号与槽
+    home_hotel_init();      //参数初始化
 }
 
 home_hotel::~home_hotel()
@@ -70,6 +74,115 @@ bool home_hotel::eventFilter(QObject *obj, QEvent *event)
 void home_hotel::home_hotel_init()
 {
     focus_flag = -1;
+    local_city = "昆明";
+    time_timer->start(60*1000); //每分钟更新一次时间
+    weather_timer->start(60*60*1000); //每小时更新一次天气
+    weather_inquiry();  //获取天气
+    update_time();  //更新时间
+}
+
+void home_hotel::signal_slots_connect()
+{
+    time_timer = new QTimer(this);      //时间更新计时
+    connect(time_timer,SIGNAL(timeout()),this,SLOT(update_time()));
+
+    weather_timer = new QTimer(this);      //天气更新计时
+    connect(weather_timer,SIGNAL(timeout()),this,SLOT(update_weather()));
+
+    weather_manager = new QNetworkAccessManager(this);
+    connect(weather_manager,SIGNAL(finished(QNetworkReply*)),this,SLOT(update_weather(QNetworkReply*)));
+}
+
+void home_hotel::update_time()
+{
+    QString year_month_day= QDateTime::currentDateTime().toString("yyyy-MM-dd"); //年月日
+    QString hour_minutes= QDateTime::currentDateTime().toString("HH:mm");   //时 分
+    ui->yyyy_mm_dd->setText(year_month_day);
+    ui->hh_mm->setText(hour_minutes);
+}
+
+void home_hotel::weather_inquiry()  //获取天气
+{
+    /*设置发送数据*/
+    char quest_array[256]="http://wthrcdn.etouch.cn/weather_mini?city=";
+    QNetworkRequest quest;
+    sprintf(quest_array,"%s%s",quest_array,local_city.toUtf8().data());
+    qDebug("请求天气数据--> URL:%s\n",quest_array);
+    quest.setUrl(QUrl(quest_array));
+    quest.setHeader(QNetworkRequest::UserAgentHeader,"RT-Thread ART");
+    weather_manager->get(quest);
+}
+void home_hotel::update_weather(QNetworkReply* reply)
+{
+    static QString wind_state;     //风向
+    static QString Temperature;    //气温
+    static QString weather_type;   //天气状况
+
+    QString all = reply->readAll();
+    QJsonParseError err;
+    QJsonDocument json_recv = QJsonDocument::fromJson(all.toUtf8(),&err);
+    qDebug() << err.error;
+    if(!json_recv.isNull())
+    {
+        QJsonObject object = json_recv.object();
+        if(object.contains("data"))
+        {
+            QJsonValue value = object.value("data");  // 获取指定 key 对应的 value
+            if(value.isObject())
+            {
+                QJsonObject object_data = value.toObject();
+                if(object_data.contains("forecast"))
+                {
+                    QJsonValue value = object_data.value("forecast");
+                    if(value.isArray())
+                    {
+                        QJsonObject today_weather = value.toArray().at(0).toObject();
+                        weather_type = today_weather.value("type").toString();
+                        QString low = today_weather.value("low").toString();
+                        QString high = today_weather.value("high").toString();
+                        Temperature = "气温:";
+                        Temperature += low.mid(low.length()-3,2) +"~"+ high.mid(high.length()-3,4);
+                        QString strength = today_weather.value("fengli").toString();
+                        strength.remove(0,9);
+                        strength.remove(strength.length()-3,3);
+                        wind_state = today_weather.value("fengxiang").toString() + ":" + strength;
+                    }
+                }
+            }
+        }
+
+    }else
+    {
+        qDebug()<<"更新天气:更新天气失败，json解析出错!!";
+        weather_type = "暂无数据";
+        Temperature = "--";
+        wind_state = "--";
+    }
+    char style_sheet_cmd[64]={"border-image: url(:/new/prefix1/pictures/"};
+    int weather_index = 0;
+
+    if(weather_type == "晴")
+        weather_index = 2;
+    else if(weather_type == "阵雨")
+        weather_index = 3;
+    else if(weather_type == "雷阵雨")
+        weather_index = 4;
+    else if(weather_type.contains("雪"))
+        weather_index = 5;
+    else if(weather_type == "多云" || weather_type == "阴" )
+        weather_index = 6;
+    else if(weather_type.contains("雷"))
+        weather_index = 7;
+    else if(weather_type.contains("雨"))
+        weather_index = 8;
+
+    sprintf(style_sheet_cmd,"%s%d.png);",style_sheet_cmd,weather_index);
+    ui->weather_log->setStyleSheet(style_sheet_cmd);  //更新天气图标
+    ui->weather_type->setText(weather_type);
+    ui->wind->setText(wind_state);
+    ui->tempreture->setText(Temperature);
+    ui->city->setText(local_city);
+    reply->deleteLater();
 }
 
 void home_hotel::on_check_in_clicked()
@@ -101,7 +214,7 @@ void home_hotel::set_lineEdit_text(int opt_code)
                 ui->phone_number->clear();
                 break;
             case -1:     //删除
-                msg.remove(msg.length()-1,1);
+                msg.chop(1);
                 ui->phone_number->setText(msg);
                 break;
             case 0:
@@ -157,7 +270,7 @@ void home_hotel::set_lineEdit_text(int opt_code)
                 ui->test_code->clear();
                 break;
             case -1:     //删除
-                msg.remove(msg.length()-1,1);
+                msg.chop(1);
                 ui->test_code->setText(msg);
                 break;
             case 0:
--- homestay/home_hotel.h
+++ homestay/home_hotel.h
@@ -4,6 +4,15 @@
 #include <QWidget>
 #include <QDebug>
 #include <QMouseEvent>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
+#include <QTimer>
+#include <QDateTime>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonParseError>
 
 namespace Ui {
 class home_hotel;
@@ -17,7 +26,15 @@ public:
     explicit home_hotel(QWidget *parent = 0);
     ~home_hotel();
     bool eventFilter(QObject *obj, QEvent *event);
-    void home_hotel_init();
+    void home_hotel_init();         //参数初始化
+    void signal_slots_connect();    //连接信号与槽
+public slots:
+    void update_time();
+
+    void weather_inquiry(); //请求天气
+
+    void update_weather(QNetworkReply *reply); //更新天气
+
 private slots:
     void on_check_in_clicked();
 
@@ -52,6 +69,15 @@ private slots:
 private:
     Ui::home_hotel *ui;
     int focus_flag;   //-1 无焦点 0 电话号码  1 验证码
+    QString local_city;   //当前所在城市
+
+    QTimer *time_timer;            //时间定时器
+    QTimer *weather_timer;         //天气定时器
+
+    QNetworkAccessManager *face_compare_manager;    //人脸比较
+    QNetworkAccessManager *weather_manager;         //天气请求
+    QNetworkAccessManager *upload_manager;          //信息上传请求
+
 };
 
 #endif // HOME_HOTEL_H
